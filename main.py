452f3ee96317f4d8726709a70fc60290133daec4
felipesantos22 <felipevs2018@gmail.com>
2026-02-09 15:06:39 -0300

    remove uvicorn

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ app = FastAPI(title="Project Task With FastAPI")
 
 app.include_router(task_router)
 
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+#if __name__ == '__main__':
+#    uvicorn.run(app, host="127.0.0.1", port=8000)
