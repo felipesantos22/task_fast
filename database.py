4f70e4b47a0651400ac51a08a4b0f89c2efd76be
felipesantos22 <felipevs2018@gmail.com>
2026-02-09 15:28:33 -0300

    change database

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -9,7 +10,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 #MySql
 #DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/dbtask"
-DATABASE_URL = "mysql+pymysql://root@localhost:3306/dbtask"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root@localhost:3306/dbtask"  # fallback local
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
 
